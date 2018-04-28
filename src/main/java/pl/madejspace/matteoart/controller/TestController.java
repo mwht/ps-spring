744c3fee5ac65e265fb33bcc276c02dffe5e8e19
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-28 16:55:44 +0200

    add TestController

--- src/main/java/pl/madejspace/matteoart/controller/TestController.java
+++ src/main/java/pl/madejspace/matteoart/controller/TestController.java
@@ -1,4 +1,13 @@
 package pl.madejspace.matteoart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping("/test")
+    public String test(Model model) {
+        return "test";
+    }
 }
