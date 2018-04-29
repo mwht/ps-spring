ac302316f6e60c049bcc71d1c5cbd82564bb987e
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-29 21:08:20 +0200

    add MenuEntry class

---
+++ src/main/java/pl/madejspace/postudencku/MenuEntry.java
@@ -0,0 +1,63 @@
+package pl.madejspace.postudencku;
+
+public class MenuEntry {
+    private long id;
+    private String name;
+    private double price;
+    private String image_url;
+    private MenuCategory category;
+
+    public MenuEntry() {
+        name = "";
+        price = 0;
+        image_url = "";
+        category = new MenuCategory();
+    }
+
+    public MenuEntry(String name, double price, String image_url, MenuCategory category) {
+        this.name = name;
+        this.price = price;
+        this.image_url = image_url;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageURL() {
+        return image_url;
+    }
+
+    public void setImageURL(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public MenuCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MenuCategory category) {
+        this.category = category;
+    }
+}
