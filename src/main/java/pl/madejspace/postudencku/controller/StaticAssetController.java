4754eb320c96dc4ea39e37738ff318615f3805bd
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-29 15:52:32 +0200

    add StaticAssetController

--- src/main/java/pl/madejspace/postudencku/controller/StaticAssetController.java
+++ src/main/java/pl/madejspace/postudencku/controller/StaticAssetController.java
@@ -1,4 +1,19 @@
 package pl.madejspace.postudencku.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
 public class StaticAssetController {
+
+    @RequestMapping(value = "/staticAsset/{assetName}", method = GET)
+    @ResponseBody
+    public String getAsset(@PathVariable String assetName, Model model) {
+        return assetName;
+    }
 }
