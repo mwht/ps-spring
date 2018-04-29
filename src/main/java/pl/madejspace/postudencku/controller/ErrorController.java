c8d9c56d281c3f0741e0b4e1aeaeddb4cd2186b5
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-29 20:48:39 +0200

    clean up controllers

--- src/main/java/pl/madejspace/postudencku/controller/ErrorController.java
+++ src/main/java/pl/madejspace/postudencku/controller/ErrorController.java
@@ -1,9 +1,12 @@
 package pl.madejspace.postudencku.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.madejspace.postudencku.Bootstrap;
 
 import java.util.Map;
 
@@ -12,11 +15,6 @@ public class ErrorController {
 
     @GetMapping("/error")
     public String error(@RequestParam Map<String,String> params, Model model) {
-        params.forEach((id,elem) -> {
-            System.out.println(id + " " + elem);
-        });
-        model.addAttribute("status", 404);
-        model.addAttribute("type","Not Found");
         return "error";
     }
 }
---
+++ src/main/java/pl/madejspace/postudencku/controller/MenuController.java
@@ -0,0 +1,13 @@
+package pl.madejspace.postudencku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MenuController {
+    @GetMapping("/menu")
+    public String menu(Model model) {
+        return "menu";
+    }
+}
