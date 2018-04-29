cdd96d7469488e4b4c5840d53432126556ef50d6
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-29 20:58:39 +0200

    clean up ErrorController

--- src/main/java/pl/madejspace/postudencku/controller/ErrorController.java
+++ src/main/java/pl/madejspace/postudencku/controller/ErrorController.java
@@ -1,12 +1,9 @@
 package pl.madejspace.postudencku.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pl.madejspace.postudencku.Bootstrap;
 
 import java.util.Map;
 
