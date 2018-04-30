6b9ab89f5fa2c08ce759b915bf342a3dd22100f4
Sebastian Madejski <madejski.s97@gmail.com>
2018-04-30 12:58:45 +0200

    enable displaying data

--- src/main/java/pl/madejspace/postudencku/controller/MenuController.java
+++ src/main/java/pl/madejspace/postudencku/controller/MenuController.java
@@ -1,21 +1,25 @@
 package pl.madejspace.postudencku.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.madejspace.postudencku.MenuEntry;
+import pl.madejspace.postudencku.dao.MenuEntryDAO;
 
 @Controller
 public class MenuController {
+
+    private ConfigurableApplicationContext context;
+
     @GetMapping("/menu")
     public String menu(Model model) {
-        @Autowired
-        JdbcTemplate jdbc = new JdbcTemplate();
-        jdbc.query("SELECT id, name, price, image_url FROM dishes AS d JOIN categories AS c ON d.category_id=c.id", null,
-                (rs, rowID) -> {
-
-                });
+        // SELECT d.id, d.name, d.price, d.image_url, c.name FROM dishes AS d JOIN categories AS c ON d.category_id=c.id;
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MenuEntryDAO menuEntryDAO = (MenuEntryDAO) context.getBean("menuEntryDAO");
+        MenuEntry entry = menuEntryDAO.findById(1);
+        model.addAttribute("entry",entry);
         return "menu";
     }
 }
